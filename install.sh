18437db9c3674f3955206b4c69a35a4b2ff50a31
Gray Programmerz <hackrefisher@gmail.com>
2019-11-04 13:38:23 +0500

    installation file
    sudo chmod +x install.sh
    sudo bash install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-apt-get update -y && apt-get install python3 python3-pip -y 
-pip3 install requests bs4 tor re2 random2 stem
+sudo apt-get update -y && sudo apt-get install python3 python3-pip -y 
+sudo pip3 install requests bs4 tor re2 random2 stem
